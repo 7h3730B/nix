7ad4060fb4291a0d1fc01264ffc75724413a00a9
7h3730B <56200898+7h3730B@users.noreply.github.com>
2022-04-15 15:07:42 +0200

    fix: removed no longer available things

--- home/programs/i3.nix
+++ home/programs/i3.nix
@@ -31,26 +31,7 @@
     };
     polybar = {
       enable = true;
-      script = ''
-        # Terminate already running bar instances.
-        killall -q polybar
-
-        # Wait until the processes have been shut down.
-        while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-        # Launch Polybar, using default config location ~/.config/polybar/config
-        # polybar topbar -r &
-        # polybar bottombar -r
-
-        # Launch an instance of polybar for every monitor connected
-        if type "xrandr"; then
-          for m in $(xrandr -q | grep " connected" | cut -d" " -f1); do
-          MONITOR=$m polybar -r topbar &
-          done
-        else
-          polybar -r topbar &
-        fi
-      '';
+      script = "polybar &";
     };
   };
 }
