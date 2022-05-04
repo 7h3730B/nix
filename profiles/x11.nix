7304561fed0aece0afed063ac05fe8f90349c764
7h3730B <56200898+7h3730B@users.noreply.github.com>
2022-05-04 19:30:48 +0200

    fix: trying again

--- profiles/x11.nix
+++ profiles/x11.nix
@@ -4,9 +4,9 @@
     layout = "eu";
     libinput.enable = true;
 
-    displayManager.xterm.enable = true;
     windowManager.bspwm.enable = true;
     displayManager.defaultSession = "none+bspwm";
+    displayManager.lightdm.enable = true;
   };
 
   services.xbanish.enable = true;
