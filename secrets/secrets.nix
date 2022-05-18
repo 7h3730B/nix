343af6fe96f5710fddb189572319fa32bb22a7be
7h3730B <56200898+7h3730B@users.noreply.github.com>
2022-05-18 20:36:47 +0200

    feat: added public keys for albedo

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,13 +1,13 @@
 let 
   users = [
-
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHG+H/iAAM1BPI4Ys/c80paJMw1RrqIEGmWNY9Gy1X8J teo@albedo"
   ];
 
   systems = {
-
+    albedo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICjNvjIYtI/DiFAZLNYXHrlhwe44dlzIREkQzZQoL3bk root@albedo";
   };
 
   keysForSystems = list: users ++ (builtin.map (s: systems."${s}") list);
-  in {
-
-  }
+in {
+  "supersecretpw.txt".publicKyes = keysForSystems [ "albedo" ];
+}
