e9a6f14c9826f528d5161d4e199da821ee6a8760
7h3730B <56200898+7h3730B@users.noreply.github.com>
2022-05-20 18:44:02 +0200

    fix: fixed type in pub key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let 
   users = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHG+H/iAAM1BPI4Ys/c80paJMw1RrqIEGmWNY9Gy1X8J teo@albedo"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHg+H/iAAM1BPI4Ys/c80paJMw1RrqIEGmWNY9Gy1X8J teo@albedo"
   ];
 
   systems = {
