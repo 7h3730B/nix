55900e8db234d139ae8b3042c32571149b4c12c6
7h3730b <56200898+7h3730B@users.noreply.github.com>
2022-05-26 18:29:53 +0200

    feat: laying ground work for pi3

--- sd-image.nix
+++ sd-image.nix
@@ -1,20 +1,19 @@
-{ ... }: {
-  nixpkgs.crossSystem.system = "armv7l-linux";
+{ pkgs, lib, ... }: {
+  nixpkgs.crossSystem.system = "aarch64-linux";
   imports = [
     <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>    
   ];
 
-  users.users.nixos.group = "nixos";
-  users.extraUsers.nixos = {
-    isSystemUser = true;
+  boot.kernelPackages = pkgs.linuxPackages_5_4;
+
+  documentation.enable = false;
+  # sdImage.compressImage = false;
+
+  systemd.services.sshd.wantedBy = lib.mkOverride 40 [ "multi-user.target" ];
+  services.sshd.enable = true;
+  users.users.root = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHg+H/iAAM1BPI4Ys/c80paJMw1RrqIEGmWNY9Gy1X8J teo@albedo"
     ];
   };
-
-  # documentation.enable = false;
-  # sdImage.compressImage = false;
-
-  # systemd.services.sshd.wantedBy = lib.mkOverride 40 [ "multi-user.target" ];
-  services.sshd.enable = true;
 }
